a7f5f9f9813abbfe70286bcecd4397f216d17ca0
Aman Sinha <1998amansinha@gmail.com>
2024-04-06 20:12:59 +0545

    Practise

--- 01_WeatherApp/index.js
+++ 01_WeatherApp/index.js
@@ -1,23 +1,22 @@
 const apiKey = "dcedfe62622d5c413f7b6e36dd2edda1";
 const apiUrl =
   "https://api.openweathermap.org/data/2.5/weather?units=metric&q=";
-
+const weatherIcon = document.querySelector(".weather-icon");
 const searchBox = document.querySelector(".search input");
 const searchBtn = document.querySelector(".search button");
-const weatherIcon = document.querySelector(".weather-icon");
 
 async function checkWeather(city) {
   const response = await fetch(apiUrl + city + `&appid=${apiKey}`);
 
   if (response.status == 404) {
-    document.querySelector(".error").style.display = "block";
     document.querySelector(".weather").style.display = "none";
+    document.querySelector(".error").style.display = "block";
   } else {
-    var data = await response.json();
+    const data = await response.json();
 
-    document.querySelector(".city").innerHTML = data.name;
     document.querySelector(".temp").innerHTML =
       Math.round(data.main.temp) + "°C";
+    document.querySelector(".city").innerHTML = data.name;
     document.querySelector(".humidity").innerHTML = data.main.humidity + "%";
     document.querySelector(".wind").innerHTML = data.wind.speed + " km/h";
 
@@ -37,7 +36,6 @@ async function checkWeather(city) {
     document.querySelector(".error").style.display = "none";
   }
 }
-
 searchBtn.addEventListener("click", () => {
   checkWeather(searchBox.value);
 });
